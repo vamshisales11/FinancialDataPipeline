d8822fb92786f6046e645c133b07da5cc8d2ea92
vamshiawsde <vamshisales11@gmail.com>
2025-10-10 19:35:18 -0400

    fourth commit

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -3,6 +3,13 @@ terraform {
   required_providers {
     aws = { source = "hashicorp/aws", version = "~> 5.50" }
   }
+  backend "s3" {
+    bucket         = "bc003-tfstate-743771860567-us-east-1"  # replace with your $TF_BUCKET
+    key            = "iac/terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "bc003-tfstate-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
